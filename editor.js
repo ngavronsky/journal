ef06bcb3b2626b2f22f800ace0e9ccd23e648377
ngavronsky <ngavronsky@gmail.com>
2017-01-16 22:09:03 -0500

    style updates

--- editor.js
+++ editor.js
@@ -2,10 +2,8 @@ var today = new Date().toDateString();
 var object = {};
 
 function textEditorData() {
-
   if (localStorage[today]){
       object = JSON.parse(localStorage[today]);
-      object.content = "What's one thing you want to improve?";
       document.getElementById('content').innerHTML=object.content;
   }
 }
