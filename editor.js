f81cc3ecd2b6f89e70aed0db888189f7d8f96953
ngavronsky <ngavronsky@gmail.com>
2017-01-17 22:16:11 -0500

    start of journal tiles

--- editor.js
+++ editor.js
@@ -13,4 +13,13 @@ function saveData() {
   localStorage[today] = JSON.stringify(object);
 }
 
+function shareData() {
+  var journal = "";
+  for (var i = 0; i < localStorage.length; i++){
+    var key = localStorage.key(i);
+    journal = JSON.parse(localStorage.getItem(key))['content'];
+  }
+  document.getElementById('journals').innerHTML = JSON.stringify(journal);
+}
+
 textEditorData();
